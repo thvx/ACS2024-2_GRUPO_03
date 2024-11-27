15a340d18e4ac0aa311aa37182942bdc20e3c75a
Overx18 <jricastica90@gmail.com>
2024-11-27 17:16:08 -0500

    Agregando codigo de sintonizacion mediante PIDTUNER y graficas

--- Diseño e Implementación/PID.m
+++ Diseño e Implementación/PID.m
@@ -5,29 +5,65 @@ l = 1;            % Longitud del péndulo
 g = 9.8;          % Gravedad
 u = 0;            % Fuerza
 
-
-% Funcion de transferencia
+% FUNCION DE TRANSFERENCIA
 numtf = [m*l];                           % Numerador
 dentf = [(M + m)*l^2, 0, -m*g*l];        % Denominador
 G=tf(numtf, dentf);                      % Funcion de transferencia
 
-% Controlador PID sin sintonizar
+% CONTROLADOR PID SIN SINTONIZAR
 K_p = 100;     %Constante proporcional
 K_i = 75;      %Constante integral
 K_d = 10;       %Constante derivativa
-C = pid (K_p, K_i, K_d);
+C_no_sintonizado= pid (K_p, K_i, K_d);
 
-% %Impresión de gráficas
-T = feedback(G,C)
+%------------------------------------------------------------------------------------------------------%
 
-% Gráfica de las raíces del PID
+% IMPRESION DE GRAFICAS
+T_no_sintonizado= feedback(G,C_no_sintonizado)
+
+% GRAFICA DE LAS RAICES DEL PID
 figure(1)
-rlocus(T)
+rlocus(T_no_sintonizado)
 title({'Raices del PID sin sintonizar'});
 
-% Gráfica de la respuesta al impulso
+% GRAFICA DE LA RESPUESTA AL IMPULSO
 figure(2)
-t=0:0.01:10; % Tiempo de simulación a 10 segundos
-impulse(T,t)
-axis([0, 5, -0.5, 0.5]);  % Límites del gráfico
-title({'Resultado respecto al Ángulo \ theta - PID: Kp=100, Ki=50, Kd=5'});
+t=0:0.01:5; % Tiempo de simulación a 10 segundos
+impulse(T_no_sintonizado,t)
+axis([0, 2, -0.2, 0.2]);  % Límites del gráfico
+title({'Resultado respecto al Ángulo \theta'});
+
+%------------------------------------------------------------------------------------------------------%
+
+pidTuner (G, C_no_sintonizado);           %PIDTUNER SINTONIZA AUTOMATICAMENTE LAS CONSTANTES(K_d, K_i, K_d) DEL PID 
+
+% CONTROLADOR PID SINTONIZADO
+K_p = 256.7083;
+K_i = 495.8264;
+K_d = 33.2269;
+C_sintonizado = pid (K_p, K_i, K_d);
+
+% IMPRESION DE GRAFICAS
+T_sintonizado= feedback(G,C_sintonizado);
+
+% GRAFICA DE LAS RAICES DEL PID
+figure(3)
+rlocus(T_sintonizado)
+title({'Lugar geometrico de las raices PID sintonizado'});
+
+% GRAFICA DE LA RESPUESTA AL IMPULSO
+figure(4)
+t=0:0.01:5; % Tiempo de simulación a 10 segundos
+impulse(T_sintonizado,t)
+axis([0, 2, -0.2, 0.2]);  % Límites del gráfico
+title({'Respuesta péndulo mejorado respecto al Ángulo \theta'});
+
+%------------------------------------------------------------------------------------------------------%
+
+% GRAFICA DE COMPARACION DE LA RESPUESTA DEL IMPULSO
+figure(5);
+impulse(T_no_sintonizado, 'b', T_sintonizado, 'r');
+legend('PID no sintonizado', 'PID sintonizado');
+title('Comparación de respuestas al impulso');
+
+

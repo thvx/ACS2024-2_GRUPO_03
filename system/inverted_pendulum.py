c10d9f81945301a04abcfe07b0753a44a60b1238
JorgeIP <jorgep.ipanaquep@gmail.com>
2024-11-29 00:42:12 -0500

    feat: cambiar cantidad de puntos para la prueba

--- system/inverted_pendulum.py
+++ system/inverted_pendulum.py
@@ -1,6 +1,5 @@
 import numpy as np
 import control as ctrl
-import matplotlib.pyplot as plt
 
 
 class InvertedPendulum:
@@ -11,25 +10,14 @@ class InvertedPendulum:
     def get_transfer_function(self):
         return ctrl.TransferFunction(self.numerator, self.denominator)
 
-    # Función para graficar la respuesta al escalón
-    def plot_step_response(self):
+    def get_step_response_data(self):
         transfer_function_system = self.get_transfer_function()
 
         # Definimos el tiempo de simulación con np.linspace
-        # Tiempo de simulación (de 0 a 10 segundos, con 100 puntos)
-        time = np.linspace(0, 10, 100)
+        # 7000 puntos en 5 segundos
+        time = np.linspace(0, 5, 7000)
 
         # Obtenemos la respuesta al escalón utilizando step_response
         time, response = ctrl.step_response(transfer_function_system, time)
 
-        # Imprimimos los valores
-        print(time)
-        print(response)
-
-        # Graficamos la respuesta
-        plt.plot(time, response)
-        plt.title("Respuesta al escalón del system de péndulo invertido")
-        plt.xlabel("Tiempo (s)")
-        plt.ylabel("Ángulo θ (rad)")
-        plt.grid(True)
-        plt.show()
+        return time, response

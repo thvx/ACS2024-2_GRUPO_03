ea2b2a7ec556cfa71f3e93fc6dc293b933ff7da3
JosueEF03 <josueef072003@gmail.com>
2024-12-09 23:45:14 -0500

    feat: explicar inicializacion de pendulo invertido

--- system/inverted_pendulum.py
+++ system/inverted_pendulum.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 class InvertedPendulum:
     def __init__(self, transfer_function_dict):
+        # Inicialización de péndulo invertido
         self.numerator = transfer_function_dict['numerator']
         self.denominator = transfer_function_dict['denominator']
         self.tf = ctrl.TransferFunction(self.numerator, self.denominator)
@@ -19,10 +20,8 @@ class InvertedPendulum:
         return time, response
 
     def simulate_with_pid(self, K_p, K_i, K_d, t_end=5, steps=500):
-        """Simula la respuesta del sistema con un controlador PID (sistema cerrado)."""
-        # Controlador PID
+        # Simulación de PID
         C = ctrl.TransferFunction([K_d, K_p, K_i], [1, 0])
-        # Sistema en lazo cerrado
         T = ctrl.feedback(self.tf * C)
         time = np.linspace(0, t_end, steps)
         time, response = ctrl.step_response(T, T=time)
